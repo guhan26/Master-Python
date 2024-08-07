baf622c03e4501865c7dd2060764718ec4bad4b6
guhan26 <92621227+guhan26@users.noreply.github.com>
2024-08-07 15:42:45 +0530

    Process.py

---
+++ PythonUdemyCourse/section_5/Understanding the if __name__ == __main__ Syntax/Process.py
@@ -0,0 +1,15 @@
+def run():
+    print("process started running...")
+
+def shut_down():
+    print("process is shutting down...")
+
+def email_admin():
+    print("process is email...")
+
+
+# Strating of Program
+if __name__ == '__main__':
+    run()
+    shut_down()
+    shut_down()
