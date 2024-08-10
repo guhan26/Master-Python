e2fb0f6e6db06a5f468abbc90dbf8a68578b89ed
guhan26 <92621227+guhan26@users.noreply.github.com>
2024-08-10 10:37:37 +0530

    SQL StatementFundamentals.sql

--- MYSQLUdemyCourse/SQL StatementFundamentals.sql
+++ MYSQLUdemyCourse/SQL StatementFundamentals.sql
@@ -5,3 +5,11 @@ SELECT EmpID,EmpName FROM students.employee;
 
 -- Distinct is keywoord used to return distinct(Unirue) values
 SELECT distinct EmpDept FROM students.employee;
+
+-- The COUNT is returning the number of rows in the table
+SELECT count(EmpDept) FROM students.employee;
+
+-- The WHERE clause is used to filter records
+SELECT * FROM students.employee
+WHERE EmpName = 'Guhan';
+WHERE EmpAge < 25;
