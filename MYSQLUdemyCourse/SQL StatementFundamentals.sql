ab5856779162c84ca436100ec61ff4f9869b26ad
guhan26 <92621227+guhan26@users.noreply.github.com>
2024-08-10 09:59:58 +0530

    SQL_Statement_Fundamentals.sql

---
+++ MYSQLUdemyCourse/SQL StatementFundamentals.sql
@@ -0,0 +1,7 @@
+
+-- Select Statement is used to view table
+SELECT * FROM students.employee;
+SELECT EmpID,EmpName FROM students.employee;
+
+-- Distinct is keywoord used to return distinct(Unirue) values
+SELECT distinct EmpDept FROM students.employee;
