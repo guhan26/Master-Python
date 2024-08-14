d25532977e956e50d8095c4adaef2f496b4a105a
guhan26 <92621227+guhan26@users.noreply.github.com>
2024-08-14 23:54:42 +0530

    GROUP BY Statements.sql

--- MYSQLUdemyCourse/GROUP BY Statements.sql
+++ MYSQLUdemyCourse/GROUP BY Statements.sql
@@ -10,23 +10,3 @@ GROUP BY EmpID,EmpName ORDER BY SUM(EmpAge) ASC;
 SELECT EmpID,SUM(EmpAge) FROM students.employee
 GROUP BY EmpID
 HAVING SUM(EmpAge) >23 
-
--- ASSESSMENT TEST 1
-
--- 1.
-SELECT customer_id,SUM(amount)
-FROM payment
-WHERE staff_id = 2
-GROUP BY customer_id
-HAVING SUM(amount) > 110;
-
--- 2.
-SELECT COUNT(*) FROM film
-WHERE title LIKE 'J%';
-
--- 3.
-SELECT first_name,last_name FROM customer
-WHERE first_name LIKE 'E%'
-AND address_id <500
-ORDER BY customer_id DESC
-LIMIT 1;
