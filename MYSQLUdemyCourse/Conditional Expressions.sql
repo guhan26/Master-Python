f75c646441fcc2be4994a27d7cf568416e7ea600
guhan26 <92621227+guhan26@users.noreply.github.com>
2024-08-14 23:53:36 +0530

    Conditional Expressions.sql

--- MYSQLUdemyCourse/Conditional Expressions.sql
+++ MYSQLUdemyCourse/Conditional Expressions.sql
@@ -1,1 +1,19 @@
+-- CASE
 
+SELECT EmpName,
+CASE 
+    WHEN(EmpID <= 5) THEN 'premium'
+    WHEN(EmpID BETWEEN 2 AND 6) THEN 'PLUS'
+    ELSE 'Normal'    
+END
+FROM students.employee;
+
+-- import and Export
+
+CREATE TABLE simple(
+a INTEGER,
+b INTEGER,
+c INTEGER
+);
+
+SELECT * FROM students.simple;
